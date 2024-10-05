34e5489e915c22b2a652e8cc1c90cb63e435a671
Aatif05-it <fazalsh15@gmail.com>
2024-10-05 12:48:40 +0530

    farid 4.py
    techspire python

--- Python/Easy/4.py
+++ Python/Easy/4.py
@@ -1,9 +1,11 @@
-message = ‘py" 
+message = "py" 
 print(message)
 
-title = "Python's "cool" feature"
+title = """Python's "cool" feature"""
+print(title)
 
-description = 'This is a Python "error example'
+description = 'This is a Python "error example"'
+print(description)
 
-sentence = “Hello, Python!”
+sentence = "Hello, Python!"
 print(sentence)
