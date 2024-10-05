518a3698fdbe2c8774223d96f035cc3afa6654b9
Aatif05-it <fazalsh15@gmail.com>
2024-10-05 13:41:40 +0530

    farid medium 26.py
    techspire python

--- Python/Medium/26.py
+++ Python/Medium/26.py
@@ -1,10 +1,12 @@
-def multiply:
-    Return a*b
-a=int(input(‘enter a num’))
-b=int(input(‘enter another number’))
+def multiply(a,b):
+    return a*b
+a=int(input("enter a num: "))
+b=int(input("enter another number: "))
+print(f"The multiplication of a,b is: {multiply(a,b)}")
 
 def square(num):
     return num * num
 
+print(f"The square of num is: {square(5)}")
 
-square  
+
